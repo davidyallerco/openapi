09351a7b6bfba44f254ff791316fb028c540c9ca
davidyallerco <david.yallerco@gmail.com>
2025-04-17 08:49:35 -0500

    contract firs basico funcionando

---
+++ src/main/java/net/yallerco/openapi/SaludoController.java
@@ -0,0 +1,15 @@
+package net.yallerco.openapi;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+
+import net.yallerco.openapi.api.SaludoApi;
+
+@RestController
+public class SaludoController implements SaludoApi {
+
+    @Override
+    public ResponseEntity<String> getSaludo() {
+        return ResponseEntity.ok("¡Hola desde Contract-First!");
+    }
+}
