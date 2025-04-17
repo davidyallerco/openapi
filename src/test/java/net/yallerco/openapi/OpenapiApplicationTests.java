345d223c7da80a4c94caa9fa9cdf339e7ed7383d
davidyallerco <david.yallerco@gmail.com>
2025-04-17 08:27:24 -0500

    Proyecto inicial para contract first, aun no me funciona

---
+++ src/test/java/net/yallerco/openapi/OpenapiApplicationTests.java
@@ -0,0 +1,13 @@
+package net.yallerco.openapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
